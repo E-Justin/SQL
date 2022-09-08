695529a39f1d3c401961b05192e502b985296646
E-Justin <67754579+E-Justin@users.noreply.github.com>
2022-09-08 07:26:32 -0700

    Update dl_cities with completed trades.sql

--- dl_cities with completed trades.sql
+++ dl_cities with completed trades.sql
@@ -1,3 +1,8 @@
+"""You are given the tables below containing information on 
+   Robinhood trades and users. Write a query to list the top 
+   three cities that have the most completed trade orders in 
+   descending order. Output the city and number of orders."""
+
 WITH combined AS (
   SELECT u.user_id as uuser_id, t.user_id, u.city, t.status
   FROM trades t
