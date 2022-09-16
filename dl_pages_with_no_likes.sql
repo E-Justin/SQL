5e859e1b391798ccd8fcd212389e21d8eaf5e741
E-Justin <67754579+E-Justin@users.noreply.github.com>
2022-09-16 14:00:51 -0700

    Update dl_pages_with_no_likes.sql

--- dl_pages_with_no_likes.sql
+++ dl_pages_with_no_likes.sql
@@ -1,8 +1,8 @@
-""" Assume you are given the tables below about Facebook 
+/* Assume you are given the tables below about Facebook 
     pages and page likes. Write a query to return the page 
     IDs of all the Facebook pages that don't have any likes. 
-    The output should be in ascending order."""
-    
+    The output should be in ascending order.
+    */
     
 SELECT p.page_id
 FROM pages p
